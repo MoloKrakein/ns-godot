d2aef2b369d0e680d136a304d23d483d4e13b63f
MoloKrakein <1123atha@gmail.com>
2026-06-04 15:50:20 +0700

    remove unity old code

--- Unity Old Code/Assets/Scripts/StaticValue.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class StaticValue : MonoBehaviour
-{
-    public static bool isWin;
-}
--- Unity Old Code/Assets/Scripts/soundManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class soundManager : MonoBehaviour
-{
-    public static soundManager instance;
-}
